eaf10edd4119fe06261aacb474e10132ba204f34
Vidol vidolov <vivividolov@gmail.com>
2024-11-29 22:30:40 +0200

    done type created

--- backend/src/auth/utils/Serializer.ts
+++ backend/src/auth/utils/Serializer.ts
@@ -4,17 +4,19 @@ import { AuthService } from '../auth.service';
 import { AUTH_SERVICE } from 'src/consts/moduleNames';
 import { User } from '@prisma/client';
 
+type DoneCallback = (error?: Error | null, result?: User | null) => void;
+
 @Injectable()
 export class SessionSerializer extends PassportSerializer {
   constructor(@Inject(AUTH_SERVICE) private readonly authService: AuthService) {
     super();
   }
 
-  serializeUser(user: User, done: Function) {
+  serializeUser(user: User, done: DoneCallback) {
     done(null, user);
   }
-  async deserializeUser(payload: User, done: Function) {
+  async deserializeUser(payload: User, done: DoneCallback) {
     const user = await this.authService.findUser(payload.id);
-    return done(null, user ?? null);
+    return done(null, user);
   }
 }
