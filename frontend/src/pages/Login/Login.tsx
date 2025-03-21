971d1a416626fd254fe950aba1974ec732c10b5a
Vidol vidolov <vivividolov@gmail.com>
2025-03-22 00:07:10 +0200

    login link fixed

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Login: React.FC = () => {
   const handleGoogleLogin = () => {
-    window.location.href = `http://${
+    window.location.href = `${
       import.meta.env.VITE_BACKEND_BASE_URL
     }/api/auth/google/login`;
   };
